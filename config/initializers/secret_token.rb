2e470ccb1a81b7ac9e06bb72fb1f8b44a960c77a
David Vega <zetacu@gmail.com>
2014-03-23 23:38:52 -0600

    setup with pg and other configurations

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CruiseControl::Application.config.secret_token = 'f2320a09cee7498ee5c1d66be9de7e6ded638c98beaf323450800ae60b6b69822c93f6df4bf9e60bcf44afb922424c7b1fe837e7eda273d26687f3faa8185042'
+CruiseControl::Application.config.secret_token = Rails.env.production? ? ENV['SECRET_KEY_BASE'] : 'X' * 30
